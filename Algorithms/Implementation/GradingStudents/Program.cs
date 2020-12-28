182e365ddb794d0d444237ff95d984d31a3625bf
Marco Gartmann <marco.gartmann@ost.ch>
2020-12-28 15:16:59 +0100

    add gradingStudents challenge

---
+++ Algorithms/Implementation/GradingStudents/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<int> gradingStudents(List<int> grades)
+    {
+        var roundedGrades = new List<int>();
+        foreach(int grade in grades)
+        {
+            roundedGrades.Add(roundGrade(grade));
+        }
+
+        return roundedGrades;
+    }
+
+    private static int roundGrade(int grade)
+    {
+        int differenceToNextMultiplyOfFive = (5 - grade % 5);
+        if (grade >= 38 && differenceToNextMultiplyOfFive < 3)
+        {
+            return grade + differenceToNextMultiplyOfFive;
+        }
+
+        return grade;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        TextWriter textWriter = Console.Out;
+
+        int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> grades = new List<int>();
+
+        for (int i = 0; i < gradesCount; i++)
+        {
+            int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+            grades.Add(gradesItem);
+        }
+
+        List<int> result = Result.gradingStudents(grades);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
