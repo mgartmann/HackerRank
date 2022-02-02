6673d7ae5c5791c1b455889b53fb3d9282e9debf
Marco Gartmann <marco.gartmann@ost.ch>
2022-02-02 22:01:15 +0100

    add a very big sum solution

---
+++ Algorithms/Implementation/AVeryBigSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long aVeryBigSum(List<long> ar)
+    {
+        return ar.Sum();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+
+        long result = Result.aVeryBigSum(ar);
+
+        Console.WriteLine(result);
+    }
+}
